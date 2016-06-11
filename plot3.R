d8b44c2044735f75a55bb93d88cd7795637ea427
cherylyn-ee <cherylyn_ee@icloud.com>
2016-06-11 09:42:21 +0800

    Add files via upload

--- plot3.R
+++ plot3.R
@@ -2,7 +2,7 @@ par(mar=c(4,4,4,4))
 with(powercon,plot(Day,as.numeric(as.character(Sub_metering_1)),type="l", ylab = "Energy sub metering",col="black"))
 with(powercon,lines(Day,as.numeric(as.character(Sub_metering_2)),type="l",col="red"))
 with(powercon,lines(Day,as.numeric(as.character(Sub_metering_3)),type="l",col="blue"))
-
+legend("topright",c("Sub_1","Sub_2","Sub_3"),col=(c("black","red","blue")),lty = 1)
 # to copy out - block when running plot4
 # dev.copy(png,file="plot3.png")
 # dev.off()
